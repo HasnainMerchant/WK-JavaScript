7a13663fdb4b44b71c9e213be6a92d6bd354c0b4
Hasnain Merchant <hasnainmerchant0@gmail.com>
2022-07-18 12:33:39 +0530

    Calculator Update

--- first.js
+++ first.js
@@ -1,18 +1,40 @@
-x = 5
-y = 15
-z = x * y
-console.log(z)
+// x = 5
+// y = 15
+// z = x * y
+// console.log(z)
 
-var person = {
-    firstname : 'John',
-    lastname : 'Doe',
-    age : 43,
-    gender : 'Male'
-}
-console.log(person.firstname)
-// Shows Only In Browser
-console.warn(person.firstname)
-console.error(person.firstname)
+// var person = {
+//     firstname : 'John',
+//     lastname : 'Doe',
+//     age : 43,
+//     gender : 'Male'
+// }
+// console.log(person.firstname)
+// // Shows Only In Browser
+// console.warn(person.firstname)
+// console.error(person.firstname)
 
-msg = `He is ${person.firstname} a ${person.age} old ${person.gender}`
-console.log(msg)
+// msg = `He is ${person.firstname} a ${person.age} old ${person.gender}`
+// console.log(msg)
+
+// const arr= [2, 3, 4]
+// let [x, y, z] = arr
+// console.log(x, y, z)
+// console.log(arr)
+
+const question = new Map([
+    ["Nagraj", 100],
+    ["Football", ["Hello", "World"]],
+    [102, "Football"],
+    [[1,2], ["hello", "world"]],
+]);
+
+console.log(question)
+let key_itr = question.values()
+console.log(typeof(key_itr.next().value));
+console.log(typeof(key_itr.next().value));
+console.log(typeof(key_itr.next().value));
+console.log(typeof(key_itr.next().value));
+// values = question.get([1,2])
+// console.log(values)
+// console.log(question.values());
---
+++ scope.js/Scope_HandsOn.js
@@ -0,0 +1,12 @@
+function fun(){
+    a = 12;
+    var b = 15;
+}
+{
+    d = 20;
+    let e = 30;
+}
+// console.log(a);
+// console.log(b);
+console.log(d);
+console.log(e);
