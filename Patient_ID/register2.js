a2334d5421b3de71c5b908087a423f49fa1c2ab3
Hasnain Merchant <hasnainmerchant0@gmail.com>
2022-07-20 15:45:06 +0530

    Done Somwething

--- Patient_ID/register2.js
+++ Patient_ID/register2.js
@@ -1,5 +1,6 @@
 patient_list = []
 function registerPatient(){
+    document.getElementById("details").innerText = "";
     let id = Math.floor(Math.random() * 5);
     let firstname = document.getElementById("fname").value;
     let lastname = document.getElementById("lname").value;
@@ -7,11 +8,18 @@ function registerPatient(){
     let age = document.getElementById("age").value;
     patient = [id, firstname, lastname, gender, age]
     patient_list.push(patient);
-    let pateintSet = new Set(patient_list);
-    //document.getElementById("divTable").innerText = "";
-    pateintSet.forEach(displayPatientList);
+    let patientSet = new Set(patient_list);
+    document.getElementById("fname").innerText = "";
+    document.getElementById("lname").innerText = "";
+    document.getElementById("gender").innerText = "";
+    document.getElementById("age").innerText = "";
+    if (patientSet.has(id)){
+        alert("ID Is Taken");
+    }
+    patientSet.forEach(displayPatientList);
 }
 let displayPatientList = function(patient){
+    document.getElementById("details").innerText += " " + `${patient}`;
     // var table = document.getElementById("myTable");
     // var row = table.insertRow(1);
     // var cell1 = row.insertCell(0);
@@ -25,9 +33,9 @@ let displayPatientList = function(patient){
     // cell3.innerHTML = `${patient[2]}`;
     // cell4.innerHTML = `${patient[3]}`;
     // cell2.innerHTML = "NEW CELL2";
-    document.getElementById("p_id").innerText += " " + `${patient[0]}`;
-    document.getElementById("firstname").innerText += " " + `${patient[1]}`;
-    document.getElementById("lastname").innerText += " " + `${patient[2]}`;
-    document.getElementById("p_gender").innerText += " " + `${patient[3]}`;
-    document.getElementById("p_age").innerText += " " + `${patient[4]}`;
+    // document.getElementById("p_id").innerText += " " + `${patient[0]}`;
+    // document.getElementById("firstname").innerText += " " + `${patient[1]}`;
+    // document.getElementById("lastname").innerText += " " + `${patient[2]}`;
+    // document.getElementById("p_gender").innerText += " " + `${patient[3]}`;
+    // document.getElementById("p_age").innerText += " " + `${patient[4]}`;
 };
