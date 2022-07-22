158cc97e6e34632634a5603cd0cbc99306f755b9
Hasnain Merchant <hasnainmerchant0@gmail.com>
2022-07-22 10:57:21 +0530

    Done and Done

--- Patient_ID/register2.js
+++ Patient_ID/register2.js
@@ -1,6 +1,5 @@
 patient_list = []
 function registerPatient(){
-    document.getElementById("details").innerText = "";
     let id = Math.floor(Math.random() * 5);
     let firstname = document.getElementById("fname").value;
     let lastname = document.getElementById("lname").value;
@@ -18,21 +17,33 @@ function registerPatient(){
     }
     patientSet.forEach(displayPatientList);
 }
+
+patient_id = []
+patient_list = []
+function generateRandomID(){
+    let patientIdSet = new Set(patient_id);
+    let id = Math.floor(Math.random() * 5) + 1;
+    if(patientIdSet.has(id)){
+        alert("ID Already Exits");
+    }
+    else{
+        document.getElementById("details").innerText = "";
+        patient_id.push(id);
+        let firstname = document.getElementById("fname").value;
+        let lastname = document.getElementById("lname").value;
+        let gender = document.getElementById("gender").value;
+        let age = document.getElementById("age").value;
+        document.getElementById("fname").innerText = "";
+        document.getElementById("lname").innerText = "";
+        document.getElementById("gender").innerText = "";
+        document.getElementById("age").innerText = "";
+        patient = [id, firstname, lastname, gender, age];
+        patient_list.push(patient);
+        patient_list.forEach(displayPatientList);
+    }
+}
 let displayPatientList = function(patient){
     document.getElementById("details").innerText += " " + `${patient}`;
-    // var table = document.getElementById("myTable");
-    // var row = table.insertRow(1);
-    // var cell1 = row.insertCell(0);
-    // var cell2 = row.insertCell(1);
-    // var cell3 = rwo.insertCell(2);
-    // var cell4 = row.insertCell(3);
-
-    // // Add some text to the new cells:
-    // cell1.innerHTML = `${patient[0]}`;
-    // cell2.innerHTML = `${patient[1]}`;
-    // cell3.innerHTML = `${patient[2]}`;
-    // cell4.innerHTML = `${patient[3]}`;
-    // cell2.innerHTML = "NEW CELL2";
     // document.getElementById("p_id").innerText += " " + `${patient[0]}`;
     // document.getElementById("firstname").innerText += " " + `${patient[1]}`;
     // document.getElementById("lastname").innerText += " " + `${patient[2]}`;
